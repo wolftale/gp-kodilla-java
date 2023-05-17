35097cadef2ccbde99d121cc23ed298381e343d6
GP <gp@GPs.local>
2023-05-17 20:52:20 +0200

    6.2 Zadanie: pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(10, 6);
+        if (resultAdd == 16) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int resultSubtract = calculator.subtract(10, 6);
+        if (resultSubtract == 4) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
