7dfb5f716482cae4305d4d3f1ca553dba397c977
GP <greg@ZZB-2.local>
2023-06-05 12:07:34 +0200

    Test

--- kodilla-stream/src/main/java/com/kodilla/stream/book/BookDirectory.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/book/BookDirectory.java
@@ -15,6 +15,8 @@ public final class BookDirectory {
         theBookList.add(new Book("Ryan Talley", "Gangsters and kings", 2007, "0005"));
         theBookList.add(new Book("Madelynn Carson", "Unity without duty", 2007, "0006"));
         theBookList.add(new Book("Giancarlo Guerrero", "Enemies of eternity", 2009, "0007"));
+        theBookList.add(new Book("Laura Hillenbrand", "Unbroken", 2010, "0008"));
+
     }
 
     public List<Book> getList() {
