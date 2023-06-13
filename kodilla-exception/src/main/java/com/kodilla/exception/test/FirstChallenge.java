fee583d67765d5c4a3c0073b5c3fcdb98bb11da9
GP <gp@GP.local>
2023-06-13 18:55:37 +0200

    8.2 Zadanie: try! catch! finally

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,23 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Nie można dzielić przez zero!");
+        } finally {
+            System.out.println("Koniec programu");
+        }
+    }
+}
