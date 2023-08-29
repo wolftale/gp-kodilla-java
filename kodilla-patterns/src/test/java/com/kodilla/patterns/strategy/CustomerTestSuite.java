92a4bfdf4407d867f195d3fe9e110f1e7d6d7fa9
GP <gp@GP.local>
2023-08-29 17:20:27 +0200

    Update

--- kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/CustomerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/CustomerTestSuite.java
@@ -1,6 +1,5 @@
 package com.kodilla.patterns.strategy;
 
-import com.kodilla.patterns.strategy.predictors.AggressivePredictor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
