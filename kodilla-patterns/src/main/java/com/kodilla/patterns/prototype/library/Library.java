979f4351f043557ac9c38d5a715ed576f4418e08
GP <gp@GP.local>
2023-08-29 13:27:58 +0200

    Zadanie 15.2. Klonujemy Biblioteke

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,41 @@
+package com.kodilla.patterns.prototype.library;
+
+import com.kodilla.patterns.prototype.Prototype;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Library extends Prototype<Library> {
+
+    private String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library)super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library)super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for (Book book : books) {
+            clonedLibrary.getBooks().add(new Book(book.getTitle(), book.getAuthor(), book.getPublicationDate()));
+        }
+        return clonedLibrary;
+    }
+}
