ebb9d4d87317c38a15d62bce0757fe0f5431f65f
GP <gp@GP.local>
2023-08-29 16:01:54 +0200

    Update

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/CorporateCustomer.java
@@ -0,0 +1,8 @@
+package com.kodilla.patterns.strategy;
+
+public final class CorporateCustomer extends Customer {
+    public CorporateCustomer(String name) {
+        super(name);
+        this.buyPredictor = new BalancedPredictor();
+    }
+}
