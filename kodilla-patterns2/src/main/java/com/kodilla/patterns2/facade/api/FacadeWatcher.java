e86d709a669015ab22140a3c179b71447c94a5b6
GP <gp@gpms.local>
2023-11-26 13:37:20 +0100

    Zadanie 24.3. Dodajemy logowanie żądań do fasady

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/FacadeWatcher.java
@@ -0,0 +1,20 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FacadeWatcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(order, userId) && target(object)")
+    public void logEvent(OrderDto order, long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + "\nArgs: UserID: " + userId + "\nItems: " + order);
+    }
+}
