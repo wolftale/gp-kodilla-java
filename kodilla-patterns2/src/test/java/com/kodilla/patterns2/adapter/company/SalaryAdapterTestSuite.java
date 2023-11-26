1cbccfc0a8154e2e27ca785692aa0e9a8fbc6637
GP <gp@gpms.local>
2023-11-26 16:08:06 +0100

    Update

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/company/SalaryAdapterTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.patterns2.adapter.company;
+
+import com.kodilla.patterns2.adapter.company.oldhrsystem.Workers;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalaryAdapterTestSuite {
+
+    @Test
+    public void testTotalSalary() {
+        // Given
+        Workers workers = new Workers();
+        SalaryAdapter salaryAdapter = new SalaryAdapter();
+        // When
+        double totalSalary = salaryAdapter.totalSalary(workers.getWorkers(), workers.getSalaries());
+        // Then
+        System.out.println(totalSalary);
+        assertEquals(27750, totalSalary, 0);
+    }
+
+}
